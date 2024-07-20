081a14dea84563b964e60b023c0b0a20dec8e295
Wennie Tabib <wtabib@gmail.com>
2024-07-20 12:35:44 -0400

    Fix compilation errors for Ubunut

--- cmake/guts_utilsConfig.cmake
+++ cmake/guts_utilsConfig.cmake
@@ -2,5 +2,11 @@
 
 get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../" ABSOLUTE)
 set(guts_utils_INCLUDE_DIR "${PACKAGE_PREFIX_DIR}/guts_utils/include")
-set(guts_utils_LIBRARIES "${PACKAGE_PREFIX_DIR}/guts_utils/libguts_utils.dylib")
+if (APPLE)
+  set(guts_utils_LIBRARIES "${PACKAGE_PREFIX_DIR}/guts_utils/libguts_utils.dylib")
+elseif(UNIX)
+  set(guts_utils_LIBRARIES "${PACKAGE_PREFIX_DIR}/guts_utils/libguts_utils.so")
+else()
+  message(FATAL_ERROR "Operating system not supported")
+endif()
 set(guts_utils_LIBRARY_DIR "${PACKAGE_PREFIX_DIR}/guts_utils")
